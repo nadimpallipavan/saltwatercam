f5b8847feb5aafa4bc4a0eb52cebdd3840db7a79
Pavan Raju <nadimpallipavanms@gmail.com>
2026-06-18 18:02:49 -0400

    Update brand logos to use text logo only

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer({ setPage }) {
       <div className="footerLogo">
         <button onClick={() => setPage('home')} aria-label="Go home">
           <img 
-            src="logo-circle.png" 
+            src="logo-text.png" 
             alt={`${siteContent.brand} Logo`} 
             className="footerLogoImg" 
           />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header({ page, setPage, isAudioPlaying, onToggleAudio })
       {/* Brand logo image from mockup */}
       <button className="brand" onClick={() => handleNavClick('home')} aria-label="Go home">
         <img 
-          src="logo-circle.png" 
+          src="logo-text.png" 
           alt={`${siteContent.brand} Logo`} 
           className="brandLogoImg" 
         />
