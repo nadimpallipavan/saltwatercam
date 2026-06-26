e89d1d34616f0a6d4868d94ce82288c55bdcda50
Pavan Raju <nadimpallipavanms@gmail.com>
2026-06-26 00:55:54 -0400

    Update live stream YouTube video ID to TteSjALao0E

--- src/components/LiveStream.jsx
+++ src/components/LiveStream.jsx
@@ -297,7 +297,7 @@ export default function LiveStream({ addShells, shells, currentUser }) {
           {hasInteracted && (
             <iframe
               id="yt-live-stream"
-              src="https://www.youtube.com/embed/qi0mY6zVQnY?enablejsapi=1&autoplay=1&mute=1&controls=0&rel=0&showinfo=0&iv_load_policy=3&loop=1&playlist=qi0mY6zVQnY"
+              src="https://www.youtube.com/embed/TteSjALao0E?enablejsapi=1&autoplay=1&mute=1&controls=0&rel=0&showinfo=0&iv_load_policy=3&loop=1&playlist=TteSjALao0E"
               title="Live Underwater Stream"
               className="streamBgImage"
               style={{
