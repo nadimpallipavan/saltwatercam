b51b959a0bd1ee9b2f4aa3025583fd49f7d231eb
Pavan Raju <nadimpallipavanms@gmail.com>
2026-06-26 00:23:42 -0400

    fix: host underwater MP4 video locally to bypass all CORS and Safari restrictions

--- src/components/LiveStream.jsx
+++ src/components/LiveStream.jsx
@@ -254,7 +254,7 @@ export default function LiveStream({ addShells, shells, currentUser }) {
           />
           <video
             ref={videoRef}
-            src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
+            src="/underwater.mp4"
             className="streamBgImage"
             autoPlay loop muted playsInline
             crossOrigin="anonymous"
